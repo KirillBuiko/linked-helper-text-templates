b5743b576fd6b092c1496998a032a814e167fc69
KirillBuiko <k.buiko04@mail.ru>
2023-07-16 05:39:55 +0700

    Variable button name change and moved (?)

--- src/components/controls/VariableButton.tsx
+++ src/components/controls/VariableButton.tsx
@@ -1,11 +1,11 @@
-import styles from './VariablesItem.module.scss';
+import styles from './VariableButton.module.scss';
 import ControlButton from "@/components/controls/ControlButton";
 
 type Props = {
     variable: string,
     onVariableClick: () => void
 }
-export default function VariablesItem(props: Props) {
+export default function VariableButton(props: Props) {
     return (
         <div className={styles.wrapper}>
             <ControlButton onClick={props.onVariableClick}>{props.variable}</ControlButton>
--- src/components/widget-template-edit/VariablesBlock.tsx
+++ src/components/widget-template-edit/VariablesBlock.tsx
@@ -1,13 +1,13 @@
-import VariablesItem from "@/components/widget-template-edit/VariablesItem";
+import VariableButton from "@/components/controls/VariableButton";
 import styles from './VariablesBlock.module.scss';
 
 type Props = { arrVarNames: string[], onVariableClick: (variable: string) => void };
 export default function VariablesBlock(props: Props) {
     const variablesList = props.arrVarNames.map((v, ind) =>
         <>
-            <VariablesItem key={ind}
-                           variable={v}
-                           onVariableClick={() => props.onVariableClick(v)}/>
+            <VariableButton key={ind}
+                            variable={v}
+                            onVariableClick={() => props.onVariableClick(v)}/>
         </>
     );
     return (
